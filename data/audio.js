0dfb16edd4aaf36190650741b5181ba284653d9d
Kamil Chomiak <kamil.chomiak@outlook.com>
2022-09-01 18:02:28 -0400

    checking audio

--- data/audio.js
+++ data/audio.js
@@ -1,22 +1,22 @@
 const audio = {
   Title: new Howl({
-    src: "../assets/audio/Visager - The Final Road [Loop].mp3",
+    src: "./assets/audio/Visager - The Final Road [Loop].mp3",
     html5: true,
   }),
   Start: new Howl({
-    src: "../assets/audio/startbutton.wav",
+    src: "./assets/audio/startbutton.wav",
     html5: true,
   }),
   Map: new Howl({
-    src: "../assets/audio/Rolemusic - Itsumo no yō ni.mp3",
+    src: "./assets/audio/Rolemusic - Itsumo no yō ni.mp3",
   }),
   Punch: new Howl({
-    src: "../assets/audio/punch.wav",
+    src: "./assets/audio/punch.wav",
   }),
   SlimeDmg: new Howl({
-    src: "../assets/audio/slimeDamage.wav",
+    src: "./assets/audio/slimeDamage.wav",
   }),
   Typing: new Howl({
-    src: "../assets/audio/typing.wav",
+    src: "./assets/audio/typing.wav",
   }),
 };
